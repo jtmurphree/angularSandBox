a87a15660c697f19346916c1ada06947a2e0c227
Jason Murphree <jtmurphree@gmail.com>
2018-03-02 23:25:53 -0600

    commit example

--- app/js/app.js
+++ app/js/app.js
@@ -2,16 +2,9 @@
 /* http://docs.angularjs.org/#!angular.service */
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.widgets']).
-  run(['$route', '$window', '$rootScope', function($route, $window, $rootScope) {
+angular.module('myApp',['myApp.controllers','ui.grid']);
 
-    $route.when('/view1', {template: 'partials/partial1.html', controller: MyCtrl1});
-    $route.when('/view2', {template: 'partials/partial2.html', controller: MyCtrl2});
-    $route.otherwise({redirectTo: '/view1'});
-
-    var self = this;
-
-    $rootScope.$on('$afterRouteChange', function(){
-      $window.scrollTo(0,0);
-    });
-  }]);
+angular.module('myApp').run(function($rootScope){
+	$rootScope.title='Famous Books';
+	$rootScope.name="Root Scope"
+});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,29 @@
-'use strict';
 /* App Controllers */
 
+angular.module('myApp.controllers', []).controller('SiteController', function($scope){
+	$scope.publisher='SitePoint';
+	$scope.type="Web Development";
+	$scope.name="Scope for SiteController";
+});
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
-
-
-function MyCtrl2() {
-}
-MyCtrl2.$inject = [];
+angular.module('myApp.controllers').controller('BookController', ['$scope', function($scope){
+	$scope.books = ['Jump Start HTML5', 'Jump Start CSS', 'Jump Start Responsive Web Design'];
+	$scope.name="Scope for BookController";
+	$scope.addToWishList=function(book){
+		$scope.wishListCount++;
+	};
+	$scope.wishListCount=0;
+	$scope.myData = [{firstName: "Cox", lastName: "Carney"},
+        			 {firstName: "Jason", lastName: "Murphree"}];
+        				
+    $scope.gridOptions = { data : 'myData',
+    					columnDefs: [{field: 'firstName', displayName: 'First Name', width: "50%"},
+    								 {field: 'lastName', displayName: 'Last Name', width: "50%"}] };
+            
+	$scope.$watch('wishListCount',function(newValue, oldValue){
+		console.log('called '+newValue+' times');
+		if(newValue==2){
+			alert('Great! you have 2 items in your wish list. Time to buy what you love.');
+		}
+	});
+}]);
